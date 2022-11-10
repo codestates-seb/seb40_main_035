ca4b40faf963de97cbdf04886595266da040683f
GeonwonSeo <pisatan@gmail.com>
2022-11-10 15:15:46 +0900

    MemberControllerTest 작성

--- server/src/test/java/com/codestates/mainproject/domain/member/controller/MemberControllerTest.java
+++ server/src/test/java/com/codestates/mainproject/domain/member/controller/MemberControllerTest.java
@@ -16,6 +16,9 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -33,8 +36,7 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -62,9 +64,10 @@ class MemberControllerTest {
                 "hgd@gmail.com", "홍길동", "hgd1234@","2ndPass");
         String content = gson.toJson(postDto);
         MemberResponseDto responseDto= new MemberResponseDto(
-                1L,"hgd@gmail.com", "hgd1234@", "홍길동","길동이입니다","1티어",
+                1L,"hgd@gmail.com", "hgd1234!", "홍길동","길동이입니다","1티어",
                 List.of("Java", "SpringMVC", "SpringJPA"),
-                List.of("미디어"),"github.com/honggildong",
+                List.of("미디어"),
+                "github.com/honggildong",
                 List.of(new Article()),
                 List.of(new Heart()),
                 LocalDateTime.of(2022,11,10,11,30),
@@ -142,7 +145,7 @@ class MemberControllerTest {
         String content = gson.toJson(patchDto);
 
         MemberResponseDto responseDto =
-                new MemberResponseDto(1L, "hgd@gmail.com","pass2nd","고길동",
+                new MemberResponseDto(1L, "hgd@gmail.com","hgd1234@","고길동",
                         "홍길동아닙니다", "2티어", List.of("Python","R","Pytorch"),List.of("헬스케어"),
                         "github.com/gogildong",List.of(new Article()),List.of(new Heart())
                         ,LocalDateTime.of(2022,11,10,11,30),LocalDateTime.now());
@@ -218,11 +221,138 @@ class MemberControllerTest {
     }
 
     @Test
-    void getMember() {
+    void getMember() throws Exception{
+        //given
+        long memberId = 1;
+
+        MemberResponseDto responseDto = new MemberResponseDto(memberId, "hgd@gmail.com", "hgd1234!", "홍길동","길동이입니다","1티어",
+                List.of("Java", "SpringMVC", "SpringJPA"),
+                List.of("미디어"),"github.com/honggildong",
+                List.of(new Article()),
+                List.of(new Heart()),
+                LocalDateTime.of(2022,11,10,11,30),
+                LocalDateTime.now());
+
+        given(memberService.findMember(Mockito.anyLong()))
+                .willReturn(new Member());
+
+        given(mapper.memberToMemberResponseDto(Mockito.any(Member.class)))
+                .willReturn(responseDto);
+
+        //when
+        ResultActions actions =
+                mockMvc.perform(
+                        get("/members/{member-id", memberId)
+                                .accept(MediaType.APPLICATION_JSON)
+                );
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.memberId").value(memberId))
+                .andDo(document("get-member",
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(parameterWithName("member-id").description("회원 식별자")),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("회원 이메일"),
+                                        fieldWithPath("data.password").type(JsonFieldType.STRING).description("회원 비밀번호"),
+                                        fieldWithPath("data.name").type(JsonFieldType.STRING).description("회원 이름"),
+                                        fieldWithPath("data.description").type(JsonFieldType.STRING).description("회원 소개"),
+                                        fieldWithPath("data.level").type(JsonFieldType.STRING).description("회원 티어"),
+                                        fieldWithPath("data.stack").type(JsonFieldType.ARRAY).description("회원 기술스택"),
+                                        fieldWithPath("data.field").type(JsonFieldType.ARRAY).description("회원 산업분야"),
+                                        fieldWithPath("data.github").type(JsonFieldType.STRING).description("회원 깃허브"),
+                                        fieldWithPath("data.article").type(JsonFieldType.ARRAY).description("회원 작성글"),
+                                        fieldWithPath("data.hearts").type(JsonFieldType.ARRAY).description("회원 좋아요"),
+                                        fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("생성 날짜"),
+                                        fieldWithPath("data.modifiedAt").type(JsonFieldType.STRING).description("수정 날짜")
+                                )
+                        )
+                ));
     }
 
     @Test
-    void getMembers() {
+    void getMembers() throws Exception{
+        //given
+        int page = 1;
+        int size = 5;
+
+        List<Member> members = List.of(new Member(), new Member(), new Member());
+
+        given(memberService.findMembers(Mockito.anyInt(), Mockito.anyInt()))
+                .willReturn(new PageImpl<>(members, PageRequest
+                                .of(page-1, size, Sort.by("memberId")
+                                        .descending()), members.size()));
+
+        given(mapper.membersToMemberResponseDtos(Mockito.anyList()))
+                .willReturn(List.of(
+                        new MemberResponseDto(1L, "hgd@gmail.com", "hgd1234!", "홍길동","길동이입니다","1티어",
+                                List.of("Java", "SpringMVC", "SpringJPA"),
+                                List.of("미디어"),"github.com/honggildong",
+                                List.of(new Article()),
+                                List.of(new Heart()),
+                                LocalDateTime.of(2022,11,10,11,30),
+                                LocalDateTime.now()),
+                        new MemberResponseDto(2L, "ggd@gmail.com", "ggd1234@", "고길동","길동이아닙니다","2티어",
+                                List.of("Python","R","Pytorch"),
+                                List.of("헬스케어"),"github.com/gogildong",
+                                List.of(new Article()),
+                                List.of(new Heart()),
+                                LocalDateTime.of(2022,11,10,11,30),
+                                LocalDateTime.now()),
+                       new MemberResponseDto(3L, "kgd@gmail.com", "hgd1234#", "김길동","길동","3티어",
+                               List.of("Python", "SpringMVC", "Pytorch"),
+                               List.of("제조"),"github.com/kimgildong",
+                               List.of(new Article()),
+                               List.of(new Heart()),
+                               LocalDateTime.of(2022,11,10,11,30),
+                               LocalDateTime.now())
+                    )
+                );
+
+        //when
+        ResultActions actions =
+                mockMvc.perform(
+                        get("/members/?page={page}&size={size}", page, size)
+                                .accept(MediaType.APPLICATION_JSON)
+                );
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andDo(document("get-members",
+                        preprocessResponse(prettyPrint()),
+                        requestParameters(List.of(parameterWithName("page").description("페이지 번호"),
+                                parameterWithName("size").description("페이지 크기"))),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.ARRAY).description("결과 데이터"),
+                                        fieldWithPath("data[].memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data[].email").type(JsonFieldType.STRING).description("회원 이메일"),
+                                        fieldWithPath("data[].password").type(JsonFieldType.STRING).description("회원 비밀번호"),
+                                        fieldWithPath("data[].name").type(JsonFieldType.STRING).description("회원 이름"),
+                                        fieldWithPath("data[].description").type(JsonFieldType.STRING).description("회원 소개"),
+                                        fieldWithPath("data[].level").type(JsonFieldType.STRING).description("회원 티어"),
+                                        fieldWithPath("data[].stack").type(JsonFieldType.ARRAY).description("회원 기술스택"),
+                                        fieldWithPath("data[].field").type(JsonFieldType.ARRAY).description("회원 산업분야"),
+                                        fieldWithPath("data[].github").type(JsonFieldType.STRING).description("회원 깃허브"),
+                                        fieldWithPath("data[].article").type(JsonFieldType.ARRAY).description("회원 작성글"),
+                                        fieldWithPath("data[].hearts").type(JsonFieldType.ARRAY).description("회원 좋아요"),
+                                        fieldWithPath("data[].createdAt").type(JsonFieldType.STRING).description("생성 날짜"),
+                                        fieldWithPath("data[].modifiedAt").type(JsonFieldType.STRING).description("수정 날짜"),
+
+                                        fieldWithPath("pageInfo").type(JsonFieldType.OBJECT).description("페이지 정보"),
+                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("페이지 번호"),
+                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("페이지 크기"),
+                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("총 갯수"),
+                                        fieldWithPath("pageInfo.totalPages").type(JsonFieldType.NUMBER).description("총 페이지 수")
+                                )
+                        )
+                ));
     }
 
     @Test
