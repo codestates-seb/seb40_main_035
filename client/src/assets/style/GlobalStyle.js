8aad0ac558295850b7ceff0746890c60d10c167e
JungInHa <juudyy99@gmail.com>
2022-11-15 11:15:05 +0900

    chore: 클라이언트 초기 설정

---
+++ client/src/assets/style/GlobalStyle.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+import Variables from './GlobalVariable';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0; 
+    font-family: 'Noto Sans KR', sans-serif;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  :root {
+    ${Variables};
+  }
+
+  ul, ol, li{
+    list-style-type: none;
+  }
+
+  a, a:hover, a:active, a:visited {
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
---
+++ client/src/assets/style/GlobalVariable.js
@@ -0,0 +1,10 @@
+import { css } from 'styled-components';
+
+const Variables = css`
+  --purple: #775cbb;
+  --purple-light: #f6f6fd;
+  --black: #2b2b2b;
+  --grey-dark: #4f4f4f;
+`;
+
+export default Variables;
