75b54f7155580ae2cc636e4f36f77ba8f3a60c56
Jiwon <sjiwon9303@gmail.com>
2022-11-17 01:06:15 +0900

    feat: 셀렉트 박스 기능 구현

--- client/src/components/SelectLevel.js
+++ client/src/components/SelectLevel.js
@@ -3,11 +3,22 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { BsFillCaretDownFill } from 'react-icons/bs';
 
+const WholeContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
 const DropdownContainer = styled.div`
   display: flex;
   justify-content: center;
   &:hover {
     cursor: pointer;
+  }
+
+  span {
+    font-size: 13px;
   }
 `;
 
@@ -15,10 +26,10 @@ const DropdownBody = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 6px 14px;
-  border: solid 1px var(--purple);
+  padding: 5px 14px;
+  border: solid 1px #cfcfe9;
   background-color: var(--purple-light);
-  width: 210px;
+  width: 180px;
   border-radius: 20px;
   #down-icon {
     color: var(--purple);
@@ -29,21 +40,18 @@ const DropdownMenu = styled.ul`
   justify-content: center;
   align-items: center;
   display: ${(props) => (props.isActive ? `block` : `none`)};
-  width: 210px;
+  width: 180px;
   background-color: var(--purple-light);
   position: absolute;
   border: solid 1px var(--purple);
   border-radius: 8px;
-  font-size: 15px;
 `;
 
-const DropdownItemContainer = styled.li`
+const DropdownItem = styled.li`
   display: flex;
-  font-size: 15px;
-  font-weight: 500;
   justify-content: space-between;
   align-items: center;
-  padding: 9px 14px;
+  padding: 5px 14px;
   border-bottom: solid 1px var(--purple);
   border-top: none;
   &:hover {
@@ -71,30 +79,69 @@ const SelectLevel = () => {
   const [isActive, setIsActive] = useState(false);
   // 선택된 데이터 저장
   const [selectedValue, setSelectedValue] = useState(null);
+  const selectInput = useRef();
+
+  // 드롭다운 토글 기능
+  const onActiveToggle = useCallback(() => {
+    setIsActive((prev) => !prev);
+  }, []);
+  // 드롭다운 기능
+  const onSelect = useCallback((e) => {
+    const targetId = e.target.id;
+
+    if (targetId === 'item_name') {
+      setSelectedValue(e.target.parentElement.innerText);
+    } else if (targetId === 'item') {
+      setSelectedValue(e.target.innerText);
+    }
+    setIsActive((prev) => !prev);
+  }, []);
+
+  // 외부 영역 클릭 시 창 닫기
+  const handleClickOutSide = (e) => {
+    if (isActive && !selectInput.current.contains(e.target)) {
+      setIsActive(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isActive) document.addEventListener('mousedown', handleClickOutSide);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutSide);
+    };
+  });
+
+  const onCloseBox = () => {
+    setIsActive(false);
+    selectInput.current.focus();
+    setSelectedValue(null);
+  };
 
   return (
-    <DropdownContainer>
-      <DropdownBody>
-        {selectedValue ? (
-          <>
-            <span>{selectedValue} </span>
-            <BsFillCaretDownFill id="down-icon" />
-          </>
-        ) : (
-          <>
-            <span>숙련도를 선택해주세요.</span>
-            <BsFillCaretDownFill id="down-icon" />
-          </>
-        )}
-      </DropdownBody>
-      <DropdownMenu isActive={isActive}>
-        {selectOptions.map((item) => (
-          <DropdownItemContainer id="item" key={item.id}>
-            <span id="item_name">{item.value}</span>
-          </DropdownItemContainer>
-        ))}
-      </DropdownMenu>
-    </DropdownContainer>
+    <WholeContainer>
+      <DropdownContainer ref={selectInput}>
+        <DropdownBody onClick={onActiveToggle}>
+          {selectedValue ? (
+            <>
+              <span>{selectedValue} </span>
+              <BsFillCaretDownFill id="down-icon" />
+            </>
+          ) : (
+            <>
+              <span>숙련도를 선택해주세요.</span>
+              <BsFillCaretDownFill id="down-icon" />
+            </>
+          )}
+        </DropdownBody>
+        <DropdownMenu isActive={isActive}>
+          {selectOptions.map((item) => (
+            <DropdownItem id="item" key={item.id} onClick={onSelect}>
+              <span id="item_name">{item.value}</span>
+            </DropdownItem>
+          ))}
+        </DropdownMenu>
+      </DropdownContainer>
+    </WholeContainer>
   );
 };
 
