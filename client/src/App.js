e8a1bf5f719a87356820f71dde70098127330c2b
2seb2 <yuo231@naver.com>
2022-11-18 14:44:53 +0900

    feat: 라우팅 설정 및 파일이름 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import Header from './components/Header';
-// import { Routes, Route } from 'react-router-dom';
-// import ArticleDetail from './pages/ArticleDetail';
-// import ArticleEdit from './pages/ArticleEdit';
-// import ArticleWrite from './pages/ArticleWrite';
-// import Main from './pages/Main';
-// import MyPage from './pages/MyPage';
-// import MyPageEdit from './pages/MyPageEdit';
-// import Profile from './pages/Profile';
-// import SignUp from './pages/SignUp';
+import { Routes, Route } from 'react-router-dom';
+import ArticleDetail from './pages/ArticleDetail';
+import ArticleEdit from './pages/ArticleEdit';
+import ArticleWrite from './pages/ArticleWrite';
+import Main from './pages/Main';
+import MyPage from './pages/MyPage';
+import MyPageEdit from './pages/MyPageEdit';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  height: 62px;
+`;
 
 function App() {
   return (
     <>
-      <Header />
-      {/* <Routes>
+      <HeaderContainer>
+        <Header />
+      </HeaderContainer>
+      <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/write" element={<ArticleWrite />} />
         <Route path="/article/edit/:id" element={<ArticleEdit />} />
@@ -22,7 +29,7 @@ function App() {
         <Route path="/mypage/edit/:id" element={<MyPageEdit />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/signup" element={<SignUp />} />
-      </Routes> */}
+      </Routes>
     </>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 const HeaderContainer = styled.header`
   width: 100%;
+  height: 62px;
   display: flex;
   align-items: center;
   justify-content: space-between;
