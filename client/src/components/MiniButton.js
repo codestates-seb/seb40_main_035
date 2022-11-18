220b4058346f8c90139f2759f0eb41cc3a03d3b7
JungInHa <juudyy99@gmail.com>
2022-11-18 15:08:30 +0900

    design: 기본버튼/미니버튼 css 수정 및 내부 텍스트/onClick기능 props로 연결

--- client/src/components/MiniButton.js
+++ client/src/components/MiniButton.js
@@ -1,29 +1,32 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  width: 76px;
   height: 36px;
   background-color: var(--purple-light);
-  padding: 8px 8px 10px;
+  padding: 8px 10px;
   border-radius: 8px;
   border: none;
   color: var(--purple);
   border: 1px solid var(--purple);
   font-size: 15px;
   font-weight: 700;
+  line-height: 18px;
+  white-space: nowrap;
+  transition: 300ms ease-in-out;
+
   &:hover {
-    background-color: var(--purple);
-    color: var(--purple-light);
-  }
-  &:focus,
-  &:active {
     background-color: var(--purple-medium);
     color: var(--purple);
   }
+
+  &:active {
+    background-color: var(--purple);
+    color: var(--purple-light);
+  }
 `;
 
-const MiniButton = () => {
-  return <Button>테스트</Button>;
+const MiniButton = ({ text, onClick }) => {
+  return <Button onClick={onClick}>{text}</Button>;
 };
 
 export default MiniButton;
