7b90e612f515d802036b316ebd4c61f92d9ab8fe
daldalchoi <vocolate1990@gmail.com>
2022-11-18 14:16:56 +0900

    Feat : 라우터 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import Header from './components/Header';
+// import { Routes, Route } from 'react-router-dom';
+// import ArticleDetail from './pages/ArticleDetail';
+// import ArticleEdit from './pages/ArticleEdit';
+// import ArticleWrite from './pages/ArticleWrite';
+// import Main from './pages/Main';
+// import MyPage from './pages/MyPage';
+// import MyPageEdit from './pages/MyPageEdit';
+// import Profile from './pages/Profile';
+// import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <>
       <Header />
+      {/* <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/write" element={<ArticleWrite />} />
+        <Route path="/article/edit/:id" element={<ArticleEdit />} />
+        <Route path="/article/:id" element={<ArticleDetail />} />
+        <Route path="/mypage/:id" element={<MyPage />} />
+        <Route path="/mypage/edit/:id" element={<MyPageEdit />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes> */}
     </>
   );
 }
---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,5 @@
+const SignUp = () => {
+  return <div>ggg</div>;
+};
+
+export default SignUp;
