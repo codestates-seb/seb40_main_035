fee5b0fd47bc496802296358c1a79e50e91bc1dd
2seb2 <yuo231@naver.com>
2022-11-22 20:24:41 +0900

    feat: 마이페이지 1차 구현 완료

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,5 +1,283 @@
+import styled from 'styled-components';
+import ProfileCard from '../components/ProfileCard';
+import SkillStackView from '../components/SkillStackView';
+import InterestView from '../components/InterestView';
+import ArticleCard from '../components/ArticleCard';
+import {
+  currentUserState,
+  recruitedArticlesState,
+  likedArticlesState,
+  userProfileState,
+  skillStackViewState,
+  interestViewState,
+} from '../atom/atom';
+import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Container = styled.div`
+  min-height: calc(100vh - 62px); //전체화면-헤더 높이
+  width: fit-content;
+  color: var(--black);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .profile-card {
+    margin-top: 40px;
+  }
+
+  .skill-stack {
+    border-radius: 8px;
+    background-color: white;
+    width: 762px;
+    margin-top: 20px;
+    padding: 20px 30px;
+  }
+
+  .interest {
+    border-radius: 8px;
+    background-color: white;
+    width: 762px;
+    margin-top: 20px;
+    padding: 20px 30px;
+  }
+
+  .article-area {
+    width: calc(100vw - 340px);
+    min-width: 762px;
+  }
+
+  .article-menu {
+    padding: 60px 0 0 0;
+    display: flex;
+  }
+
+  .menu:first-child {
+    border-right: 1px solid var(--purple-medium);
+    margin-right: 20px;
+  }
+
+  .menu {
+    background-color: transparent;
+    padding: 0 20px 0 0;
+    text-align: start;
+    font-size: 18px;
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  .active-menu {
+    color: var(--purple);
+
+    .menu-content-count {
+      color: var(--black);
+    }
+  }
+
+  .menu-content-count {
+    margin-top: 5px;
+    font-size: 15px;
+    font-weight: normal;
+  }
+
+  .article-list {
+    /* min-height: 100px; */
+    margin-bottom: 100px;
+  }
+
+  .article-list > * {
+    margin: 30px 30px 0 0;
+  }
+`;
+
 const MyPage = () => {
-  return <div>ddd</div>;
+  const [activeMenu, setActiveMenu] = useState(0);
+
+  const currentUser = useRecoilValue(currentUserState); //로그인 후, 응답으로 받아 오는 멤버아이디
+  const [profileData, setProfileData] = useRecoilState(userProfileState); //프로필카드
+  const [skillStackView, setSkillStackView] =
+    useRecoilState(skillStackViewState); //기술스택
+  const setInterestView = useSetRecoilState(interestViewState); //관심분야
+  const [recruitedArticles, setRecruitedArticles] = useRecoilState(
+    recruitedArticlesState,
+  ); //모집 프로젝트
+  const [likedArticles, setLikedArticles] = useRecoilState(likedArticlesState); //좋아요 프로젝트
+
+  useEffect(() => {
+    axios.get(`/members/${currentUser.memberId}`).then((res) => {
+      // 프로필카드 상태 set
+      const userProfile = {
+        memberId: res.data.data.memberId,
+        email: res.data.data.email,
+        name: res.data.data.name,
+        description: res.data.data.description,
+        level: res.data.data.level,
+        github: res.data.data.github,
+      };
+      setProfileData(userProfile);
+
+      // 기술스택 상태 set
+      for (let el of res.data.data.skills) {
+        if (el.skillSort === '프론트엔드') {
+          setSkillStackView([
+            {
+              tabTitle: '프론트엔드',
+              tabCont: [
+                ...skillStackView[0].tabCont,
+                {
+                  skillId: skillStackView[0].tabCont.length + 1,
+                  name: el.name,
+                },
+              ],
+            },
+            {
+              tabTitle: '백엔드',
+              tabCont: [...skillStackView[1].tabCont],
+            },
+            {
+              tabTitle: '기타',
+              tabCont: [...skillStackView[2].tabCont],
+            },
+          ]);
+        }
+        if (el.skillSort === '백엔드') {
+          setSkillStackView([
+            {
+              tabTitle: '프론트엔드',
+              tabCont: [...skillStackView[0].tabCont],
+            },
+            {
+              tabTitle: '백엔드',
+              tabCont: [
+                ...skillStackView[1].tabCont,
+                {
+                  skillId: skillStackView[1].tabCont.length + 1,
+                  name: el.name,
+                },
+              ],
+            },
+            {
+              tabTitle: '기타',
+              tabCont: [...skillStackView[2].tabCont],
+            },
+          ]);
+        }
+        if (el.skillSort !== '백엔드' && el.skillSort !== '프론트엔드') {
+          setSkillStackView([
+            {
+              tabTitle: '프론트엔드',
+              tabCont: [...skillStackView[0].tabCont],
+            },
+            {
+              tabTitle: '백엔드',
+              tabCont: [...skillStackView[1].tabCont],
+            },
+            {
+              tabTitle: '기타',
+              tabCont: [
+                ...skillStackView[2].tabCont,
+                {
+                  skillId: skillStackView[2].tabCont.length + 1,
+                  name: el.name,
+                },
+              ],
+            },
+          ]);
+        }
+      }
+
+      //관심분야 상태 set
+      setInterestView(res.data.data.interests);
+
+      //모집프로젝트 목록 상태 set
+      setRecruitedArticles(res.data.data.articles);
+
+      //좋아요프로젝트 목록 상태 set
+      setLikedArticles(res.data.data.heartArticles);
+    });
+  }, []);
+
+  const tabContArr = [
+    { tabTitle: '모집한 프로젝트', tabCont: recruitedArticles },
+    { tabTitle: '좋아요한 프로젝트', tabCont: likedArticles },
+  ];
+
+  const onClickMenu = (idx) => {
+    setActiveMenu(idx);
+  };
+
+  const onlinkToEdit = () => {
+    location.href = `/mypage/edit/${profileData.memberId}`;
+    // location.href = `/mypage/edit/${profileData.memberId}`;
+  };
+
+  const onMemberDelete = () => {
+    axios.delete(`/members/${currentUser.memberId}`);
+    location.href = `/`;
+  };
+
+  return (
+    <Container>
+      <div className="profile-card">
+        <ProfileCard
+          onEditProfile={() => {
+            onlinkToEdit();
+          }}
+          onDeleteProfile={() => {
+            onMemberDelete();
+          }}
+        />
+      </div>
+      <div className="skill-stack">
+        <SkillStackView />
+      </div>
+      <div className="interest">
+        <InterestView />
+      </div>
+      <div className="article-area">
+        <ul className="article-menu">
+          {tabContArr.map((menu, idx) => {
+            return (
+              // 메뉴 버튼
+              <div
+                role="presentation"
+                key={idx}
+                className={activeMenu === idx ? 'menu active-menu' : 'menu'}
+                onClick={() => onClickMenu(idx)}
+              >
+                {menu.tabTitle}
+                <div className="menu-content-count">{`총 ${menu.tabCont.length}개 프로젝트`}</div>
+              </div>
+            );
+          })}
+        </ul>
+
+        <ul className="article-list">
+          {tabContArr[activeMenu].tabCont.map((article, idx) => {
+            return (
+              <ArticleCard
+                key={idx}
+                articleId={article.articleId}
+                isCompleted={article.isCompleted}
+                title={article.title}
+                startDay={article.startDay}
+                endDay={article.endDay}
+                frontend={article.frontend}
+                backend={article.backend}
+                hashtags={article.hashtags}
+                skills={article.skills}
+                memberName={article.memberName}
+                heartCount={article.heartCount}
+                views={article.views}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </Container>
+  );
 };
 
 export default MyPage;
