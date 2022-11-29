f2a7b6f3c4ca2eec37d8deb1cc421e4b8458e2a0
2seb2 <yuo231@naver.com>
2022-11-29 13:10:46 +0900

    feat: 정보 받기를 위한 데이터 날짜형식 변경 기능 구현

--- client/src/utils/getFormatDate.js
+++ client/src/utils/getFormatDate.js
@@ -1,13 +1,15 @@
-// 객체로 저장된 날짜형식을 yyyymmdd의 포멧으로 변경
-const getFormatDate = (date) => {
-  date = String(date);
-  date = new Date(date);
-  let year = date.getFullYear(); //yyyy
-  let month = 1 + date.getMonth(); //M
-  month = month >= 10 ? month : '0' + month; //month 두자리로 저장
-  let day = date.getDate(); //d
-  day = day >= 10 ? day : '0' + day; //day 두자리로 저장
-  return `${year + '' + month + '' + day}`; //yyyymmdd
+//데이터 날짜 형식 변경해 이전 정보 받아오기 위한 날짜 형식 변경
+const getFormatDate = (specific) => {
+  let year = Number(specific.slice(0, 4));
+  let month = Number(specific.slice(4, 6));
+  if (month === 1) {
+    month = 12;
+  } else {
+    month--;
+  }
+  let day = Number(specific.slice(6));
+
+  return new Date(year, month, day);
 };
 
 export default getFormatDate;
