fd43b6741f9079934b64086d655e673cea919925
Jiwon <sjiwon9303@gmail.com>
2022-12-06 03:36:32 +0900

    design: UI 디자인 구현

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -3,16 +3,57 @@ import styled from 'styled-components';
 import notFoundPage from '../assets/image/not.png';
 
 const NotFoundContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: calc(100vh - 62px);
+  margin: 0px;
+  box-sizing: border-box;
+  padding: 0;
+  min-width: fit-content;
+
   img {
     width: 80%;
     margin-bottom: 20px;
     height: 100%;
   }
   .not-found-wrapper {
+    margin-top: 80px;
+    margin-bottom: 80px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 25px;
   }
   button {
+    background-color: #c9c9ff;
+    border: 5px solid var(--purple);
+    border-radius: 10px;
+    height: 40px;
+    font-size: 15px;
+    font-weight: 600;
+    color: var(--purple);
+    width: auto;
+    height: auto;
+    padding: 5px;
+    text-align: center;
+    @media screen and (max-width: 720px) {
+      font-size: 0.8rem;
+    }
+    &:hover,
+    &:focus,
+    &:active {
+      transition: ease-in-out;
+      transition-duration: 1s;
+      transform: scale(1.1);
+    }
   }
   .not-found-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 `;
 
