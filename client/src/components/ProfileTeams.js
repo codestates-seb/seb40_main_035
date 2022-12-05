efcd685725ff136a90146925c50740947d55d045
Jiwon <sjiwon9303@gmail.com>
2022-12-06 03:33:06 +0900

    design: UI 디자인 구현

--- client/src/components/ProfileTeams.js
+++ client/src/components/ProfileTeams.js
@@ -2,9 +2,20 @@ import styled from 'styled-components';
 import avatar from '../assets/image/userAvatar.png';
 
 const ProfileContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  margin-top: 20px;
+  padding: 15px;
+  text-align: center;
   img {
+    width: 120px;
   }
   .name {
+    padding: 3px;
   }
   .part {
   }
@@ -14,11 +25,11 @@ const ProfileTeams = ({ name, part }) => {
   return (
     <ProfileContainer>
       <div>
-        <div className="about-profile-img">
+        <div className="profile-img">
           <img alt="profile" src={avatar} />
         </div>
-        <div className="about-name">{name}</div>
-        <div className="about-part">{part}</div>
+        <div className="name">{name}</div>
+        <div className="part">{part}</div>
       </div>
     </ProfileContainer>
   );
