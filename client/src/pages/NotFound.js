b33d4d34ccbb39234a1c22adb78c65873f45b5a8
Jiwon <sjiwon9303@gmail.com>
2022-12-06 18:30:51 +0900

    fix: 버튼 이름 변경

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -71,7 +71,7 @@ const NotFound = () => {
               navigate(`/`);
             }}
           >
-            back to home
+            메인으로 돌아가기
           </button>
         </div>
       </div>
