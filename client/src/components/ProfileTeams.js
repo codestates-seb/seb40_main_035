313616a4d4c36727497d90e588a8d57080de7362
Jiwon <sjiwon9303@gmail.com>
2022-12-06 18:34:15 +0900

    feat: map함수로 컴포넌트 렌더 변경, 팀원 미모지 추가

--- client/src/components/ProfileTeams.js
+++ client/src/components/ProfileTeams.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
-import avatar from '../assets/image/userAvatar.png';
+// import avatar from '../assets/image/userAvatar.png';
+import jw from '../assets/image/jw.png';
+import sb from '../assets/image/sb.png';
+import dh from '../assets/image/dh.jpg';
+import kw from '../assets/image/kw.jpg';
+import ji from '../assets/image/ji.png';
+import ch from '../assets/image/ch.png';
+import yk from '../assets/image/yk.png';
 
 const ProfileContainer = styled.div`
   width: 100%;
@@ -11,25 +18,108 @@ const ProfileContainer = styled.div`
   margin-top: 20px;
   padding: 15px;
   text-align: center;
+
+  .team-members-wrapper {
+  }
+  .profile-img {
+  }
+  .team-members {
+  }
   img {
-    width: 120px;
+  }
+  a {
   }
   .name {
-    padding: 3px;
   }
-  .part {
+
+  .profile-img-body {
   }
 `;
 
-const ProfileTeams = ({ name, part }) => {
+const ProfileTeams = () => {
+  const teamBackEnd = [
+    {
+      name: '서건원',
+      part: 'BackEnd',
+      imgSrc: kw,
+      github: 'https://github.com/GeonwonSeo',
+    },
+    {
+      name: '박영기',
+      part: 'BackEnd',
+      imgSrc: yk,
+      github: 'https://github.com/park-yeong-ki',
+    },
+    {
+      name: '박찬현',
+      part: 'BackEnd',
+      imgSrc: ch,
+      github: 'https://github.com/J4mbo9',
+    },
+  ];
+
+  const teamFrontEnd = [
+    {
+      name: '손지원',
+      part: 'FrontEnd',
+      imgSrc: jw,
+      github: 'https://github.com/Sonjiwon0',
+    },
+    {
+      name: '이세비',
+      part: 'FrontEnd',
+      imgSrc: sb,
+      github: 'https://github.com/2seb2',
+    },
+    {
+      name: '최동환',
+      part: 'FrontEnd',
+      imgSrc: dh,
+      github: 'https://github.com/DalDalChoi',
+    },
+    {
+      name: '하정인',
+      part: 'FrontEnd',
+      imgSrc: ji,
+      github: 'https://github.com/JungInHa',
+    },
+  ];
   return (
     <ProfileContainer>
-      <div>
-        <div className="profile-img">
-          <img alt="profile" src={avatar} />
-        </div>
-        <div className="name">{name}</div>
-        <div className="part">{part}</div>
+      <div className="team-members-wrapper">
+        {teamBackEnd.map((member, idx) => {
+          return (
+            <a href={member.github} key={idx} target="_blank" rel="noreferrer">
+              <div className="team-members">
+                <div className="profile-img">
+                  <div className="profile-img-body">
+                    <img alt="profile" src={member.imgSrc}></img>
+                  </div>
+                </div>
+                <div className="name">{member.name}</div>
+                <div className="part">{member.part}</div>
+              </div>
+            </a>
+          );
+        })}
+      </div>
+
+      <div className="team-members-wrapper">
+        {teamFrontEnd.map((member, idx) => {
+          return (
+            <a href={member.github} key={idx} target="_blank" rel="noreferrer">
+              <div className="team-members" key={idx}>
+                <div className="profile-img">
+                  <div className="profile-img-body">
+                    <img alt="profile" src={member.imgSrc}></img>
+                  </div>
+                </div>
+                <div className="name">{member.name}</div>
+                <div className="part">{member.part}</div>
+              </div>
+            </a>
+          );
+        })}
       </div>
     </ProfileContainer>
   );
