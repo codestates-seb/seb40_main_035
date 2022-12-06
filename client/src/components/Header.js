cac97e28b7ff9ad42e6eadb2b01ece33cbaa1949
Jiwon <sjiwon9303@gmail.com>
2022-12-06 18:30:29 +0900

    fix: About 페이지 메뉴 이름을 <팀 소개>로 변경

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -209,7 +209,7 @@ const Header = () => {
           <ul className="gnb">
             <li>
               <NavBtn $color={menu === 1} onClick={onAbout}>
-                사이트 소개
+                팀 소개
               </NavBtn>
             </li>
             <li>
